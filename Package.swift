a3c55382fc7484fdc14c150b458f3cbf56cb8d66
Didier Brun <db@brundidier.com>
2024-08-27 12:15:43 +0200

    List all files

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "ModuleFirebaseStorage",
             targets: ["ModuleFirebaseStorage"]),
     ],
+    dependencies: [
+           .package(url: "https://github.com/firebase/firebase-ios-sdk.git",  from: "1.1.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
--- Sources/ModuleFirebaseStorage/ModuleFirebaseStorage.swift
+++ Sources/ModuleFirebaseStorage/ModuleFirebaseStorage.swift
@@ -1,5 +1,18 @@
+import FirebaseStorage
+
 public enum ModuleFirebaseStorage {
     public static func helloWorlds(){
-        print("HelloWords")
+        ModuleFirebaseStorage.listRootFiles()
     }
-}
+    
+    private static func listRootFiles() async {
+        let storage = Storage.storage()
+        let reference = storage.reference()
+        
+        do {
+            let result = try await reference.listAll()
+            for item in result.items {
+                print(item)
+            }
+        }
+    }
