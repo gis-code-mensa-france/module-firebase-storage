3789b3abff6153b5d4273d399f4c2f1aa15e8ea6
Didier Brun <db@brundidier.com>
2024-08-27 12:37:08 +0200

    Fix listRootFiles

--- Sources/ModuleFirebaseStorage/ModuleFirebaseStorage.swift
+++ Sources/ModuleFirebaseStorage/ModuleFirebaseStorage.swift
@@ -2,23 +2,18 @@ import FirebaseStorage
 
 @available(iOS 15.0, *)
 public enum ModuleFirebaseStorage {
-    public static func helloWorlds(){
-        Task {
-            await ModuleFirebaseStorage.listRootFiles()
-        }
-    }
-    
-    private static func listRootFiles() async {
+    /**
+     List root files
+     */
+    public static func listRootFiles() async -> [StorageReference]{
         let storage = Storage.storage()
         let reference = storage.reference()
         
         do {
             let result = try await reference.listAll()
-            for item in result.items {
-                print(item)
-            }
+            return result.items
         } catch {
-            
+            return []
         }
     }
 }
